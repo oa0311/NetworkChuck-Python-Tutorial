6c84ae918477c7d3170934c4d24580ea28156011
oa0311 <88689448+oa0311@users.noreply.github.com>
2021-08-18 23:05:48 +0000

    End of Episode 1 - Python Tutorial

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-print("Hello there!!!")
-print("I am Iron Man")
-print("No, I am Tony Stark")
-print("No, blah blah")
+#printing single strings with several print functions.
+#print("Hello there!!!")
+#print("I am Iron Man")
+#print("No, I am Tony Stark")
+#print("No, blah blah")
+
 #Pound sign is how you comment in Python.
+
 #This comment is just to test the Version Control
+
+#printing a Multiline string with one print function.
+#print("""I'm Iron Man
+#No, I am Tony Stark
+#No, I am Thanos!""")
+
+#printing multiple strings with a single print function.
+#print("I am Iron Man. \n" + "No, I am Iron Man. \n" + "Whateve...")
+
+#printing the same string multiple times
+print("I am Batman\n" * 11)
